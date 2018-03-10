c4c8df31663bfb8026d9a0962ee3ec66bcef9845
markutley <mark.utley.713@gmail.com>
2018-03-10 09:37:43 -0500

    left/right alignment succes

--- etchasketch.js
+++ etchasketch.js
@@ -1,7 +1,17 @@
-for(i = 0; i < 16; i++){
+for(i = 0; i < 256; i++){
   var div = document.createElement("div");
   function appendDiv(div){
     document.getElementById("container").appendChild(div);
   };
+  div.setAttribute("id",i + 1);
   appendDiv(div);
+};
+
+for(i = 1; i < 257; i++){
+  var r = Math.floor((i - 1)/16) + 1;
+  if(i - ((r-1)*16) < 9){
+    document.getElementById(i).style.cssFloat = "left";
+  } else{
+  document.getElementById(i).style.cssFloat = "right";
+  }
 }
