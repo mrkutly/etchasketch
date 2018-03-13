f0fc8468aa0b036162ba4bd0cc85fe5aff1a3d5e
markutley <mark.utley.713@gmail.com>
2018-03-13 17:55:01 -0400

    complete styling

--- etchasketch.js
+++ etchasketch.js
@@ -51,21 +51,3 @@ resetButton.addEventListener("click", function(){
     });
   }
 });
-
-
-
-
-
-//this is a loop I wrote to float every set of 8 items alternating left and right.
-//It wasn't the right thing to do, but I am proud of the code...
-//for(i = 1; i < 257; i++){
-//  var r = Math.floor((i - 1)/16) + 1;
-//  if(i - ((r-1)*16) < 9){
-//    document.getElementById(i).style.cssFloat = "left";
-//  } else{
-//  document.getElementById(i).style.cssFloat = "right";
-//  };
-//  if((i-1) % 16 == 0){
-//    document.getElementById(i).style.clear = "left";
-//  }
-// }
